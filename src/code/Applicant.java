56142e08db0cc3e5bf6f523a5822f4683ed9f0ac
Mitsun <119714219+Mitsun126@users.noreply.github.com>
2025-03-28 01:02:04 +0800

    Add files via upload

---
+++ src/code/Applicant.java
@@ -0,0 +1,13 @@
+package code;
+
+public class Applicant extends User {
+
+	public Applicant() {
+		super();
+	}
+
+	public Applicant(String name, String NRIC, int age, String marital_status) {
+		super(name, NRIC, age, marital_status);
+	}
+
+}
---
+++ src/code/HDB_Officer.java
@@ -0,0 +1,13 @@
+package code;
+
+public class HDB_Officer extends User {
+
+	public HDB_Officer() {
+		super();
+	}
+
+	public HDB_Officer(String name, String NRIC, int age, String marital_status) {
+		super(name, NRIC, age, marital_status);
+	}
+
+}
