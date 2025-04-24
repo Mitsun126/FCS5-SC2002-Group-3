46a158235418e45561bae002f5fc170175c27466
Mitsun <119714219+Mitsun126@users.noreply.github.com>
2025-04-24 23:08:20 +0800

    Update Applicant.java

--- Group Assignment/src/code/Applicant.java
+++ Group Assignment/src/code/Applicant.java
@@ -61,15 +61,16 @@ public class Applicant extends User {
      * @param projects List of all available projects
      * @return List of eligible open projects
      */
-    public ArrayList<Project> viewOpenProjects(ArrayList<Project> projects){
-        ArrayList<Project> openProjects = new ArrayList<>();
-        for (Project project: projects) {
-            if (project.get_visibility() && eligibilityChecker.isEligibleForProjectApplicant(this, project)) {
-                openProjects.add(project);
-            }
-        }
-        return openProjects;
-    }
+   public ArrayList<Project> viewOpenProjects(ArrayList<Project> projects){
+		ArrayList<Project> openProjects = new ArrayList<>();
+		for (Project project: projects) {
+			if (project.get_visibility() && eligibilityChecker.isEligibleForProjectApplicant(this, project)) {
+				openProjects.add(project);
+			}
+		}
+		
+		return openProjects;
+	}
 
     /** @return the project the applicant has applied for */
     public Project getAppliedProject() {
@@ -82,21 +83,21 @@ public class Applicant extends User {
      * @param project The project to apply for
      */
     public void applyForProject(Project project) {
-        if (appliedProject != null) {
-            System.out.println("You have already applied for a project: " + appliedProject.get_ProjectName());
-            return;
-        }
-
-        if (!eligibilityChecker.isEligibleForProjectApplicant(this, project)) {
-            System.out.println("You are not eligible to apply for this project!");
-            return;
-        }
-
-        appliedProject = project;
-        applicationStatus = "Pending";
-        project.addApplicant(this);
-        System.out.println("Application submitted for this project!");
-    }
+		if (appliedProject != null) {
+			System.out.println("You have already applied for a project: " + appliedProject.get_ProjectName());
+			return;
+		}
+		
+		if (!eligibilityChecker.isEligibleForProjectApplicant(this, project)) {
+			System.out.println("You are not eligible to apply for this project!");
+			return;
+		}
+		
+		appliedProject = project;
+		applicationStatus = "Pending";
+		project.addApplicant(this);
+		System.out.println("Application submitted for this project!");
+	}
 
     /** @return current application status */
     public String getApplicationStatus() {
@@ -116,13 +117,15 @@ public class Applicant extends User {
      * Displays details of the applied project.
      */
     public void viewAppliedProject() {
-        if (appliedProject == null) {
-            System.out.println("You have not applied for any project!");
-        } else {
-            System.out.println("Project: " + appliedProject.get_ProjectName());
-            System.out.println("Status: " + applicationStatus);
-        }
-    }
+		if (appliedProject == null) {
+			System.out.println("You have not applied for any project!");
+			return;
+		}
+		else {
+			System.out.println("Project: " + appliedProject.get_ProjectName());
+			System.out.println("Status: " + applicationStatus);
+		}
+	}
 
     /** @return true if the applicant has requested withdrawal */
     public boolean getWithdrawalStatus() {
@@ -142,20 +145,20 @@ public class Applicant extends User {
      * @return true if withdrawal was successful
      */
     public boolean withdrawApplication() {
-        if (appliedProject == null) {
-            System.out.println("Applicant not applied for any project!");
-            return false;
-        } else if (!withdrawal_status) {
-            System.out.println("Applicant did not request for withdrawal!");
-            return false;
-        }
-
-        appliedProject = null;
-        applicationStatus = "";
-        withdrawal_status = false;
-        System.out.println("Application withdrawn successfully!");
-        return true;
-    }
+		if (appliedProject == null) {
+			System.out.println("Applicant not applied for any project!");
+			return false;
+		} else if (withdrawal_status  == false) {
+			System.out.println("Applicant did not request for withdrawal!");
+			return false;
+		}
+		
+		appliedProject = null;
+		applicationStatus = "";
+		withdrawal_status = false;
+		System.out.println("Application withdrawn successfully!");
+		return true;
+	}
 
     /**
      * Submits a new enquiry to a project.
@@ -164,25 +167,25 @@ public class Applicant extends User {
      * @param message The enquiry message
      */
     public void submitEnquiry(Project project, String message) {
-        Enquiry newEnquiry = new Enquiry(this.get_Nric(), project.get_ProjectName(), message);
-        enquiries.add(newEnquiry);
-        project.add_Enquiry(newEnquiry);
-        System.out.println("Enquiry Submitted Successfully!");
-    }
+		Enquiry newEnquiry = new Enquiry(this.get_Nric(), project.get_ProjectName(), message);
+		enquiries.add(newEnquiry);
+		project.add_Enquiry(newEnquiry);
+		System.out.println("Enquiry Submitted Successfully!");
+	}
 
     /**
      * Displays all enquiries submitted by the applicant.
      */
     public void viewEnquiries() {
-        if (enquiries.isEmpty()) {
-            System.out.println("No Enquiries Found!");
-            return;
-        }
-
-        for (Enquiry e: enquiries) {
-            System.out.println(e);
-        }
-    }
+		if (enquiries.isEmpty()) {
+			System.out.println("No Enquiries Found!");
+			return;
+		}
+		
+		for (Enquiry e: enquiries) {
+			System.out.println(e);
+		}
+	}
 
     /**
      * Edits an existing enquiry by ID.
@@ -191,15 +194,15 @@ public class Applicant extends User {
      * @param newMessage  New message for the enquiry
      */
     public void editEnquiry(int enquiryID, String newMessage) {
-        for (Enquiry e: enquiries) {
-            if (e.getEnquiryID() == enquiryID) {
-                e.setEnquiryText(newMessage);
-                System.out.println("Enquiry Updated!");
-                return;
-            }
-        }
-        System.out.println("Enquiry Not Found!");
-    }
+		for (Enquiry e: enquiries) {
+			if (e.getEnquiryID() == enquiryID) {
+				e.setEnquiryText(newMessage);
+				System.out.println("Enquiry Updated!");
+				return;
+			}
+		}
+		System.out.println("Enquiry Not Found!");
+	}
 
     /**
      * Deletes an enquiry from the applicant and project records.
@@ -208,18 +211,24 @@ public class Applicant extends User {
      * @param project   Project to remove the enquiry from
      */
     public void deleteEnquiry(int enquiryID, Project project) {
-        for (int i = 0; i < enquiries.size(); i++) {
-            if (enquiries.get(i).getEnquiryID() == enquiryID) {
-                Enquiry enquiry = enquiries.get(i);
-                project.getEnquiries().remove(enquiry);
-                enquiries.remove(i);
-
-                System.out.println("Enquiry Deleted Successfully!");
-                return;
-            }
-        }
-        System.out.println("Enquiry Not Found!");
-    }
+		for (int i = 0; i< enquiries.size(); i++) {
+			if (enquiries.get(i).getEnquiryID()==enquiryID) {
+				Enquiry enquiry = enquiries.get(i);
+				if (!enquiry.getIsClosed()) {
+					project.getEnquiries().remove(enquiry);
+					enquiries.remove(i);
+				
+					System.out.println("Enquiry Deleted Successfully!\n");
+					return;
+				}
+				else {
+					System.out.println("Enquiry is already closed!\n");
+					return;
+				}
+			}
+		}
+		System.out.println("Enquiry Not Found!\n");
+	}
 
     /** @param flatType Sets the type of flat the applicant is applying for */
     public void setFlatType(String flatType) {
