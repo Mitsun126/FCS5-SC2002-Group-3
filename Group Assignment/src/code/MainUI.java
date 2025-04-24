e44727c59fddb8201dd29e0da289453d70cc731c
Mitsun <119714219+Mitsun126@users.noreply.github.com>
2025-04-24 17:52:32 +0800

    Update MainUI.java

--- Group Assignment/src/code/MainUI.java
+++ Group Assignment/src/code/MainUI.java
@@ -23,24 +23,24 @@ public class MainUI {
 			try {
 				System.out.println("Main Menu: ");
 				System.out.println("1. Login");
-		        System.out.println("2. Exit");
-		        System.out.print("Enter your choice: ");
-		        choice = Integer.parseInt(scanner.nextLine());
-		        
-		        switch(choice) {
-			        case 1:
-			        	login();
-			        	break;
-		        	case 2:
-		        		System.out.println("Exiting System...");
-		        		fileio.saveApplicant("data/ApplicantList.csv", applicants);
-		        		fileio.saveOfficer("data/OfficerList.csv", officers);
-		        		fileio.saveManager("data/ManagerList.csv", managers);
-		        		fileio.saveProject("data/ProjectList.csv", projects);
-		        		break;
-	                default:
-	                    System.out.println("Invalid choice! Please try again!");
-		        }
+			        System.out.println("2. Exit");
+			        System.out.print("Enter your choice: ");
+			        choice = Integer.parseInt(scanner.nextLine());
+			        
+			        switch(choice) {
+				        case 1:
+				        	login();
+				        	break;
+			        	case 2:
+			        		System.out.println("Exiting System...");
+			        		fileio.saveApplicant("data/ApplicantList.csv", applicants);
+			        		fileio.saveOfficer("data/OfficerList.csv", officers);
+			        		fileio.saveManager("data/ManagerList.csv", managers);
+			        		fileio.saveProject("data/ProjectList.csv", projects);
+			        		break;
+			                default:
+			                    System.out.println("Invalid choice! Please try again!");
+			        	}
 			} catch (NumberFormatException e) {
 				System.out.println("Invalid type! Please try again!");
 			} catch (Exception e) {
@@ -48,42 +48,40 @@ public class MainUI {
 				e.printStackTrace();
 			}
 		}while(choice!=2);
-		
-
 	}
 	
 	private static void login() {
 		System.out.print("Enter NRIC: ");
-        String nric = scanner.nextLine().trim().toUpperCase();
-        System.out.print("Enter Password: ");
-        String password = scanner.nextLine();
-        
-        for (Applicant a : applicants) {
-            if (a.get_Nric().equalsIgnoreCase(nric) && a.get_Password().equals(password)) {
-                System.out.println("Login successful as Applicant.");
-                new ApplicantUI(a, projects).showMenu();
-                return;
-            }
-        }
-
-        for (HDB_Officer o : officers) {
-            if (o.get_Nric().equalsIgnoreCase(nric) && o.get_Password().equals(password)) {
-                System.out.println("Login successful as HDB Officer.");
-                new OfficerUI(o, projects).showMenu();
-                return;
-            }
-        }
-
-        for (HDB_Manager m : managers) {
-            if (m.get_Nric().equalsIgnoreCase(nric) && m.get_Password().equals(password)) {
-                System.out.println("Login successful as HDB Manager.");
-                new ManagerUI(m, projects, applicants).showMenu();
-                return;
-            }
-        }
-
-        System.out.println("Login failed! Check your NRIC or password!");
-    }
+	        String nric = scanner.nextLine().trim().toUpperCase();
+	        System.out.print("Enter Password: ");
+	        String password = scanner.nextLine();
+	        
+	        for (Applicant a : applicants) {
+	            if (a.get_Nric().equalsIgnoreCase(nric) && a.get_Password().equals(password)) {
+	                System.out.println("Login successful as Applicant.");
+	                new ApplicantUI(a, projects).showMenu();
+	                return;
+	            }
+	        }
+	
+	        for (HDB_Officer o : officers) {
+	            if (o.get_Nric().equalsIgnoreCase(nric) && o.get_Password().equals(password)) {
+	                System.out.println("Login successful as HDB Officer.");
+	                new OfficerUI(o, projects).showMenu();
+	                return;
+	            }
+	        }
+	
+	        for (HDB_Manager m : managers) {
+	            if (m.get_Nric().equalsIgnoreCase(nric) && m.get_Password().equals(password)) {
+	                System.out.println("Login successful as HDB Manager.");
+	                new ManagerUI(m, projects, applicants).showMenu();
+	                return;
+	            }
+	        }
+	
+	        System.out.println("Login failed! Check your NRIC or password!");
+	    }
 	
 
 }
